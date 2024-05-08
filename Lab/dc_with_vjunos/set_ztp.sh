9953090d805a470ebc1d98195d429415257076a6
irzan-juniper-net <irzan@juniper.net>
2024-05-08 09:30:40 +0700

    update

--- Lab/dc_with_vjunos/set_ztp.sh
+++ Lab/dc_with_vjunos/set_ztp.sh
@@ -42,7 +42,8 @@ cli -c "configure; set system commit synchronize; set chassis evpn-vxlan-default
 EOF
 sudo chmod +x /containers_data/tftp/junos_custom1.sh
 
-export JUNOS_VERSION="23.4R1.10"
+#export JUNOS_VERSION="23.4R1.10"
+export JUNOS_VERSION="23.2R1.14"
 sudo sed -i -e "s/junos-version1/${JUNOS_VERSION}/" /containers_data/tftp/ztp.json
 sudo sed -i -e "s/junos-version2//" /containers_data/tftp/ztp.json
 export LINE_JUNOS=`grep -n '"junos": {' /containers_data/tftp/ztp.json | cut -f 1 -d ":"`
